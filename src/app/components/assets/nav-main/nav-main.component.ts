bf6f7490d0cd6941ada96d7cfa2e3e202f15fe44
JPromi <jonasmpr05@gmail.com>
2025-04-15 14:11:52 +0200

    add nav

--- src/app/components/assets/nav-main/nav-main.component.ts
+++ src/app/components/assets/nav-main/nav-main.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { LoStorageService } from '../../../services/local/lo-storage.service';
+import { SessionAccountResponse } from '../../../dtos/sessionAccountResponse';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import * as fa from '@fortawesome/free-solid-svg-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'asset-nav-main',
-  imports: [],
+  imports: [
+    RouterModule,
+    CommonModule,
+    TranslateModule,
+    FontAwesomeModule
+  ],
   templateUrl: './nav-main.component.html',
-  styleUrl: './nav-main.component.scss'
+  styleUrl: './nav-main.component.scss',
+  animations: [
+    trigger(
+      'fade', [
+        transition(':enter', [
+          style({ opacity: 0, transform: 'translateY(98%)' }),
+          animate('100ms', style({ opacity: 1, transform: 'translateY(100%)' }))
+        ]),
+        transition(':leave', [
+          style({ opacity: 1, transform: 'translateY(100%)' }),
+          animate('150ms', style({ opacity: 0, transform: 'translateY(98%)' }))
+        ])
+      ]
+    )
+  ]
 })
-export class NavMainComponent {
+export class NavMainComponent implements OnInit {
 
+  constructor(
+    private loStorageService: LoStorageService,
+    private elementRef: ElementRef,
+  ) { }
+
+  fa = fa;
+
+  public account: SessionAccountResponse | null = null;
+
+  public isUserMenuOpen = false;
+
+  ngOnInit(): void {
+    this.getAccount();
+  }
+
+  public toggleUserMenu() {
+    this.isUserMenuOpen = !this.isUserMenuOpen;
+  }
+
+  private getAccount() {
+    this.loStorageService.sessionAccount$.subscribe(
+      (account) => {
+        this.account = account;
+      }
+    );
+  }
+
+  @HostListener('document:click', ['$event'])
+  public onClick(event: MouseEvent) {
+    if(this.isUserMenuOpen) {
+      const userMenuElement = this.elementRef.nativeElement.querySelector('#usermenu');
+      if (userMenuElement && !userMenuElement.contains(event.target)) {
+        this.isUserMenuOpen = false;
+      }
+    }
+  }
 }
--- src/app/components/routes/main-router/main-router.component.ts
+++ src/app/components/routes/main-router/main-router.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavMainComponent } from '../../assets/nav-main/nav-main.component';
+import { LoStorageService } from '../../../services/local/lo-storage.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-router',
   imports: [
     RouterOutlet,
-    NavMainComponent
+    NavMainComponent,
+    CommonModule
   ],
   templateUrl: './main-router.component.html',
   styleUrl: './main-router.component.scss'
 })
-export class MainRouterComponent {
+export class MainRouterComponent implements OnInit {
+  constructor(
+    private loStorageService: LoStorageService,
+    private router: Router
+  ) { }
+
+  public isLoggedIn: boolean = false;
+
+  ngOnInit(): void {
+    this.checkSession();
+  }
+
+  private checkSession() {
+    this.loStorageService.sessionAccount$.subscribe(
+      (account) => {
+        if (account) {
+          this.isLoggedIn = true;
+          // User is logged in
+        } else {
+          this.router.navigate(['/barrier/login']);
+        }
+      }
+    );
+  }
 
 }
