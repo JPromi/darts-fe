5828c124e19cdf3ceb9a95fc15ed8ff16a4c89d8
JPromi <jonasmpr05@gmail.com>
2025-04-22 15:02:42 +0200

    add settings

--- src/app/components/site/setting-account-account/setting-account-account.component.ts
+++ src/app/components/site/setting-account-account/setting-account-account.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import * as fa from '@fortawesome/free-solid-svg-icons';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-setting-account-account',
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    TranslateModule
+  ],
   templateUrl: './setting-account-account.component.html',
   styleUrl: './setting-account-account.component.scss'
 })
-export class SettingAccountAccountComponent {
+export class SettingAccountAccountComponent implements OnInit {
+  fa = fa;
 
+  form: FormGroup = new FormGroup(
+    {
+      // avatar: new FormControl(),
+      // banner: new FormControl(),
+      username: new FormControl<string>("",  [Validators.required]),
+      description: new FormControl<string>(""),
+      country: new FormControl<string>(""),
+      l_web: new FormControl<string>(""),
+      l_x: new FormControl<string>(""),
+      l_instagram: new FormControl<string>(""),
+      l_github: new FormControl<string>(""),
+      l_youtube: new FormControl<string>(""),
+      l_twitch: new FormControl<string>(""),
+      l_facebook: new FormControl<string>(""),
+    }
+  );
+
+  ngOnInit(): void {
+    console.log(this.form.get('username')?.errors?.['required']);
+  }
+
+  submit() {
+    console.log(this.form.get('username')?.errors);
+    console.log(this.form.value);
+  }
 }
