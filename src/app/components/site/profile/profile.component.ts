b387fd2611f699a991325b75508b468159f48b8c
Jonas <jonasmpr05@gmail.com>
2025-05-28 08:44:20 +0200

    update profile

--- src/app/components/site/profile/profile.component.ts
+++ src/app/components/site/profile/profile.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { SessionAccountResponse } from '../../../dtos/sessionAccountResponse';
 import { LoStorageService } from '../../../services/local/lo-storage.service';
 import { ProfileResponse } from '../../../dtos/profileResponse';
 import { ProfileService } from '../../../services/profile.service';
 import { CommonModule } from '@angular/common';
 import { ErrorPageComponent } from '../../assets/error-page/error-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule } from '@ngx-translate/core';
+import * as fa from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
   imports: [
     CommonModule,
-    ErrorPageComponent
+    ErrorPageComponent,
+    FontAwesomeModule,
+    TranslateModule,
+    RouterModule
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
@@ -22,6 +28,8 @@ export class ProfileComponent implements OnInit {
     private loStorageService: LoStorageService,
     private profileService: ProfileService
   ) { }
+
+  fa = fa;
 
   public isOwnProfile = false;
   public sessionAccount: SessionAccountResponse | null = null;
